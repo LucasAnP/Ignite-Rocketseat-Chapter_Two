a0b549dfae122a2257a0a6bb1517d434c5e585fd
niubajr19 <lucas.antonio199@outlook.com>
2022-07-13 09:51:10 -0300

    Capitulo 2 - Adicionando validações nos Inputs

--- gofinances/src/components/Forms/InputForm/styles.ts
+++ gofinances/src/components/Forms/InputForm/styles.ts
@@ -3,3 +3,11 @@ import { RFValue } from "react-native-responsive-fontsize";
 import { TextInput } from "react-native";
 
 export const Container = styled.View``;
+
+export const Error = styled.Text`
+  color: ${({ theme }) => theme.colors.attention};
+  font-size: ${RFValue(14)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+
+  margin: 7px;
+`;
--- gofinances/src/screens/Register/index.tsx
+++ gofinances/src/screens/Register/index.tsx
@@ -8,9 +8,12 @@ import {
     Fields,
     TransactionsType
 } from './styles';
+
+import *  as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { TransactionTypeButton } from '../../components/Forms/TransactionTypeButton';
 import { CategorySelectButton } from '../../components/Forms/CategorySelectButton';
-import { Modal } from 'react-native';
+import { Modal, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 import { CategorySelect } from '../CategorySelect';
 import { InputForm } from '../../components/Forms/InputForm';
 import { useForm } from 'react-hook-form';
@@ -19,6 +22,11 @@ interface FormData {
     name: string;
     amount: string;
 }
+
+const schema = Yup.object().shape({
+    name: Yup.string().required('Nome é obrigatório'),
+    amount: Yup.number().typeError('Informe um valor numerico').positive('O valor não pode ser negativo').required('O valor é obrigatório')
+})
 
 export function Register() {
     const [transactionType, setTransactionType] = useState('');
@@ -29,7 +37,9 @@ export function Register() {
         name: 'Categoria'
     });
 
-    const { control, handleSubmit } = useForm();
+    const { control, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema)
+    });
 
     function handleTransactionsTypeSelect(type: 'up' | 'down') {
         setTransactionType(type);
@@ -44,6 +54,12 @@ export function Register() {
     }
 
     function handleRegister(form: FormData) {
+        if (!transactionType)
+            return Alert.alert('Selecione o tipo da transação');
+
+        if (category.key === 'category')
+            return Alert.alert('Selecione a categoria');
+
         const data = {
             name: form.name,
             amount: form.amount,
@@ -53,38 +69,46 @@ export function Register() {
         console.log('data', data);
     }
     return (
-        <Container>
-            <Header>
-                <Title>Cadastro</Title>
-            </Header>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <Container>
+                <Header>
+                    <Title>Cadastro</Title>
+                </Header>
 
-            <Form>
-                <Fields>
-                    <InputForm
-                        name={'name'}
-                        control={control}
-                        placeholder={'name'} />
-                    <InputForm
-                        name={'amount'}
-                        control={control}
-                        placeholder={'Preço'}
-                    />
-                    <TransactionsType>
-                        <TransactionTypeButton type={'up'} title={'Income'} onPress={() => handleTransactionsTypeSelect('up')} isActive={transactionType === 'up'} />
-                        <TransactionTypeButton type={'down'} title={'Outcome'} onPress={() => handleTransactionsTypeSelect('down')} isActive={transactionType === 'down'} />
-                    </TransactionsType>
-                    <CategorySelectButton title={category.name}
-                        onPress={handleOpenSelectCategoryModal}
-                    />
-                </Fields>
+                <Form>
+                    <Fields>
+                        <InputForm
+                            name={'name'}
+                            control={control}
+                            placeholder={'name'}
+                            autoCapitalize={'words'}
+                            autoCorrect={false}
+                            error={errors.name && errors.name.message}
+                        />
+                        <InputForm
+                            name={'amount'}
+                            control={control}
+                            placeholder={'Preço'}
+                            keyboardType={'numeric'}
+                            error={errors.amount && errors.amount.message}
+                        />
+                        <TransactionsType>
+                            <TransactionTypeButton type={'up'} title={'Income'} onPress={() => handleTransactionsTypeSelect('up')} isActive={transactionType === 'up'} />
+                            <TransactionTypeButton type={'down'} title={'Outcome'} onPress={() => handleTransactionsTypeSelect('down')} isActive={transactionType === 'down'} />
+                        </TransactionsType>
+                        <CategorySelectButton title={category.name}
+                            onPress={handleOpenSelectCategoryModal}
+                        />
+                    </Fields>
 
-                <Button title={'Enviar'}
-                    onPress={handleSubmit(handleRegister)}
-                />
-            </Form>
-            <Modal visible={categoryModalOpen}>
-                <CategorySelect category={category} setCategory={setCategory} closeSelectCategory={handleCloseSelectCategoryModal} />
-            </Modal>
-        </Container>
+                    <Button title={'Enviar'}
+                        onPress={handleSubmit(handleRegister)}
+                    />
+                </Form>
+                <Modal visible={categoryModalOpen}>
+                    <CategorySelect category={category} setCategory={setCategory} closeSelectCategory={handleCloseSelectCategoryModal} />
+                </Modal>
+            </Container>
+        </TouchableWithoutFeedback>
     );
 }
